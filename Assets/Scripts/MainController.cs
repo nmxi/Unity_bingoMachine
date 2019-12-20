309684d5bacae5ff5c0f9b4fd08f63c4215cadf7
nmxi <nagatsukimxi@gmail.com>
2019-12-20 23:15:56 +0900

    v1.0

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -3,31 +3,127 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using TMPro;
+using System;
 
 public class MainController : SerializedMonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _mainText;
+    [SerializeField] private float _updateInterval;
+    [SerializeField] private List<GameObject> _lottNumObjList = new List<GameObject>();
+    [SerializeField] private GameObject _lotNumObjParent;
+    [SerializeField] private GameObject _lotNumObjPrefab;
+    [SerializeField] private int _maxNum;
+
+    //private int row;
+    private int line;
+
+    private void Start()
+    {
+        //row = 0;
+        line = 0;
+    }
 
     [ContextMenu("StartLott"), Button("StartLott", ButtonSizes.Large)]
     public void StartLott()
     {
-        Debug.Log("ok");
+        Debug.Log("Start Lott");
+
+        StartCoroutine(Animation(3f));
     }
 
     /// <summary>
     /// 数字のアニメーション
     /// </summary>
     /// <returns></returns>
-    private IEnumerator Animation(int time)
+    private IEnumerator Animation(float time)
     {
         float t = 0;
         while (true)
         {
-            t += Time.deltaTime;
-            if (t > time)
+            t += _updateInterval;
+
+            var num = UnityEngine.Random.Range(1, _maxNum);
+            _mainText.text = num.ToString();
+
+            if (t > time )
+            {
+                AddUnique();
+
                 break;
+            }
+
+            yield return new WaitForSeconds(_updateInterval);
         }
 
         yield break;
+    }
+
+    [Button("AddNum", ButtonSizes.Large)]
+    public void AddNum(string text)
+    {
+        var g = Instantiate(_lotNumObjPrefab, _lotNumObjParent.transform);
+
+        var len = _lottNumObjList.Count;
+        len--;
+
+        Debug.Log(text);
+        _mainText.text = text;
+
+        g.GetComponent<RectTransform>().anchoredPosition = new Vector2(150f * (len % 8) + 210f, (-100f * line) - 300f);
+        g.GetComponent<TextMeshProUGUI>().text = text;
+        g.name = "num";
+
+        if ((len % 8) == 7) line++;
+
+        _lottNumObjList.Add(g);
+    }
+
+    /// <summary>
+    /// Adds the unique.
+    /// </summary>
+    private void AddUnique()
+    {
+        if(_lottNumObjList.Count >= _maxNum - 1)
+        {
+            Debug.Log("Game End");
+            return;
+        }
+
+        var num = UnityEngine.Random.Range(1, _maxNum);
+
+        try
+        {
+            foreach (var g in _lottNumObjList)
+            {
+                //被ったら
+                //Debug.Log(g.GetComponent<TextMeshProUGUI>().text + " " + num.ToString());
+                if (g.GetComponent<TextMeshProUGUI>().text == num.ToString())
+                {
+                    //再抽選
+                    AddUnique();
+                    return;
+                }
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        AddNum(num.ToString());
+    }
+
+    [Button("Reset", ButtonSizes.Large)]
+    public void Reset()
+    {
+        foreach (var g in _lottNumObjList)
+        {
+            Destroy(g);
+        }
+
+        _lottNumObjList.Clear();
+        _lottNumObjList.Add(new GameObject());
+
+        _mainText.text = "0";
     }
 }
