5a701029fe951850a3bccd072c7a43b0c803d104
nmxi <nagatsukimxi@gmail.com>
2019-12-20 20:48:04 +0900

    init

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using TMPro;
+
+public class MainController : SerializedMonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _mainText;
+
+    [ContextMenu("StartLott"), Button("StartLott", ButtonSizes.Large)]
+    public void StartLott()
+    {
+        Debug.Log("ok");
+    }
+
+    /// <summary>
+    /// 数字のアニメーション
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Animation(int time)
+    {
+        float t = 0;
+        while (true)
+        {
+            t += Time.deltaTime;
+            if (t > time)
+                break;
+        }
+
+        yield break;
+    }
+}
